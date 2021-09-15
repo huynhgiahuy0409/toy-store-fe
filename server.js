8427f350325d463ea3b3fe521aaa1cd819b43a25
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-09-16 02:07:28 +0700

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/toy-store'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/toy-store/'}),
+    res.sendFile('../toy-store-fe/src/index.html', {root: 'dist/toy-store/'}),
 );
 
 // Start the app by listening on the default Heroku port
