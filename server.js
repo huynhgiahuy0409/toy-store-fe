b13574850388cbfea25cda82078df49d82151cbb
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-09-16 02:43:01 +0700

    .

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/toy-store'));
+app.use(express.static('./dist/toy-store-fe'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/toy-store/'}),
+    res.sendFile('index.html', {root: 'dist/toy-store-fe/'}),
 );
 
 // Start the app by listening on the default Heroku port
